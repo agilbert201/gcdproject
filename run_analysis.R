eab4673be08f12cd4de3c4abafabfccd121bc8aa
Andrew Gilbert <andy@agilbert.net>
2015-01-19 19:16:39 -0500

    scaffold

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+# Merge training and test data sets
+# Extract mean and std dev for each measurement
+# Name the activities
+# Label the variables
+# Create 2nd data set with average of each variable for each activity and each subject
+
